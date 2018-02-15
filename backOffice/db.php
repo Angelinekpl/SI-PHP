6d6bb50813827ba64c26dea561fe8ee57d21869a
Chrystelle Poulet <33482580+ChrystellePlt@users.noreply.github.com>
2018-02-15 17:08:08 +0100

    Update db.php

--- backOffice/db.php
+++ backOffice/db.php
@@ -1,10 +1,12 @@
 <?php
 error_reporting(E_ALL);
+
 $dsn = 'mysql:host=127.0.0.1;dbname=SIFEV';
 $username = 'root';
 $password = '';
 $options = [];
 try {
+	// create connection
 	$connection = new PDO($dsn, $username, $password, $options);
 	// set the PDO error mode to exception
 } catch(PDOException $e) {
