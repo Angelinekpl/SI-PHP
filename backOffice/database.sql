a30962212e6d804478682b06ef92afd469d76bbe
José TEIXEIRA <teixeirajosejunior@gmail.com>
2018-02-15 14:08:56 +0100

    Add files via upload

--- backOffice/database.sql
+++ backOffice/database.sql
@@ -1,22 +1,19 @@
 create database SIFEV;
 
--- use SIFEV;
-
--- create table article(
--- 	id int(10) auto_increment primary key,
--- 	title varchar(100) not null,
--- 	author varchar(50) not null, 
--- 	date datetime, not null,
--- 	content text, not null,
--- 	url varchar(2000), not null
--- );
-
 CREATE TABLE `SIFEV`.`article`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(100) NOT NULL,
     `author`VARCHAR(50) NOT NULL,
-    `date` DATETIME NOT NULL,
+    `date` DATE NOT NULL,
     `content` text NOT NULL,
-    `url` VARCHAR(2000) NOT NULL,
+    `url` VARCHAR(60) NOT NULL,
     PRIMARY KEY (`id`)
+);
+
+
+
+CREATE TABLE `SIFEV`.`categories`(
+  category_id   INT NOT NULL,
+  category_name VARCHAR(25) NULL,
+  PRIMARY KEY (`category_id`)
 );
