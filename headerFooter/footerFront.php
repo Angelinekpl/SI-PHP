c57e35d4b4442b37bdb311fe53b6f7b400cd5593
José TEIXEIRA <teixeirajosejunior@gmail.com>
2018-02-15 14:11:11 +0100

    Update footerFront.php

--- headerFooter/footerFront.php
+++ headerFooter/footerFront.php
@@ -37,7 +37,7 @@
             <a href="#"><i class="fa fa-facebook"></i></a>
             <a href="#"><i class="fa fa-twitter"></i></a>
             <a href="#"><i class="fa fa-linkedin"></i></a>
-            <a target="_blank" href="https://github.com/joetxa/SI-PHP"><i class="fa fa-github"></i></a>
+            <a target="_blank" href="https://github.com/ChrystellePlt/SI-PHP"><i class="fa fa-github"></i></a>
         </div>
     </div>
 </footer>
