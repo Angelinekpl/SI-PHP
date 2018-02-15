c7a6fdbcdeb2744d98696523ffa0bcfd378fefe2
Chrystelle Poulet <33482580+ChrystellePlt@users.noreply.github.com>
2018-02-15 17:13:46 +0100

    Update delete.php

--- backOffice/delete.php
+++ backOffice/delete.php
@@ -1,6 +1,7 @@
 <?php
 require '../backOffice/db.php';
 $id = $_GET['id'];
+// sql to delete a record
 $sql = 'DELETE FROM article WHERE id=:id';
 $statement = $connection->prepare($sql);
 if ($statement->execute([':id' => $id])) {
